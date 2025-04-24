72857c600e8bbff5d6ef4a98b728aa7508697b97
Sadik B <sadikdindwar@gmail.com>
2025-04-24 22:19:41 +0530

    first commit

---
+++ src/main/java/com/sadik/git_conflicts/GitConflictsApplication.java
@@ -0,0 +1,16 @@
+package com.sadik.git_conflicts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConflictsApplication {
+
+	public static void main(String[] args) {
+		String name ="mike";
+		int x = 100;
+		int y = 200; 
+		SpringApplication.run(GitConflictsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadik/git_conflicts/GitConflictsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadik.git_conflicts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConflictsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
